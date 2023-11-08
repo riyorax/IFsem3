714b6e83dee8152d8b8b183d575a945ddee487d4
riyorax <maximiliansulistiyo@gmail.com>
2023-11-08 12:58:22 +0700

    Prak 10

--- Algoritma dan Struktur Data/Praktikum/Praktikum 10/stacklinked.c
+++ Algoritma dan Struktur Data/Praktikum/Praktikum 10/stacklinked.c
@@ -13,7 +13,6 @@ Address newNode(ElType x){
     }
     return p;
 }
- 
 
 /* ********* PROTOTYPE REPRESENTASI LOJIK STACK ***************/
 boolean isEmpty(Stack s){
@@ -21,29 +20,43 @@ boolean isEmpty(Stack s){
     return TOP(s) == NIL;
 }
 
-
 int length(Stack s){
     /* Mengirimkan banyaknya elemen stack. Mengirimkan 0 jika Stack s kosong */
-    
+    Address p = ADDR_TOP(s);
+    int counter=0;
+    while(p != NULL){
+        counter +=1;
+        p = NEXT(p);
+    }
+    return counter;
 }
-
 
 void CreateStack(Stack *s){
     /* I.S. sembarang */ 
-/* F.S. Membuat sebuah stack s yang kosong */
+    /* F.S. Membuat sebuah stack s yang kosong */
+    TOP(*s) = NIL;
 }
-
 
 void DisplayStack(Stack s){
-/* Proses : Menuliskan isi Stack, ditulis di antara kurung siku; antara dua elemen 
-    dipisahkan dengan separator "koma", tanpa tambahan karakter di depan, di tengah, 
-    atau di belakang, termasuk spasi dan enter */
-/* I.S. s boleh kosong */
-/* F.S. Jika s tidak kosong: [e1,e2,...,en] */
-/* Contoh : jika ada tiga elemen bernilai 1 (TOP), 20, 30 akan dicetak: [1,20,30] */
-/* Jika Stack kosong : menulis [] */
+    /* Proses : Menuliskan isi Stack, ditulis di antara kurung siku; antara dua elemen 
+        dipisahkan dengan separator "koma", tanpa tambahan karakter di depan, di tengah, 
+        atau di belakang, termasuk spasi dan enter */
+    /* I.S. s boleh kosong */
+    /* F.S. Jika s tidak kosong: [e1,e2,...,en] */
+    /* Contoh : jika ada tiga elemen bernilai 1 (TOP), 20, 30 akan dicetak: [1,20,30] */
+    /* Jika Stack kosong : menulis [] */
+    Address p = ADDR_TOP(s);
+    printf("[");
+    while (p != NULL){
+        if (NEXT(p) != NULL){
+            printf("%d,", INFO(p));
+        }else{
+            printf("%d", INFO(p));
+        }
+        p = NEXT(p);
+    }
+    printf("]");
 }
-
 
 void push(Stack *s, ElType x){
     /* Menambahkan x sebagai elemen Stack s */
@@ -51,14 +64,22 @@ void push(Stack *s, ElType x){
     /* F.S. x menjadi Top yang baru jika alokasi x berhasil, */
     /*      jika tidak, s tetap */
     /* Pada dasarnya adalah operasi insertFirst pada list linier */
+    Address newTop = newNode(x);
+    if(newTop != NIL){
+        NEXT(newTop) = ADDR_TOP(*s);
+        ADDR_TOP(*s) = newTop;
+    }
 }
 
-
 void pop(Stack *s, ElType *x){
-/* Menghapus Top dari Stack s */
-/* I.S. s tidak kosong */
-/* F.S. x adalah nilai elemen Top yang lama, */
-/*      elemen Top yang lama didealokasi */
-/* Pada dasarnya adalah operasi deleteFirst pada list linier */
-
+    /* Menghapus Top dari Stack s */
+    /* I.S. s tidak kosong */
+    /* F.S. x adalah nilai elemen Top yang lama, */
+    /*      elemen Top yang lama didealokasi */
+    /* Pada dasarnya adalah operasi deleteFirst pada list linier */
+    *x = TOP(*s);
+    Address del = ADDR_TOP(*s);
+    ADDR_TOP(*s) = NEXT(ADDR_TOP(*s));
+    NEXT(del) = NIL;
+    free(del);
 }
